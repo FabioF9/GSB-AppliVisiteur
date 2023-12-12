355ed4390f8d0ae9b3f95cf7c61b2390bc7ef1d1
cartoon157 <kylianinfo157@gmail.com>
2023-12-12 14:34:00 +0100

    aizr

--- front/new_structure.py
+++ front/new_structure.py
@@ -75,18 +75,22 @@ class Index_page(QtWidgets.QWidget):
         request = requests.get(f'http://127.0.0.1:8000/rapport/visiteur/{appStack.user.id}',headers=appStack.user.headers)
         all_rapports = request.json()
         button_dict = {}
+        suppr_dict = {}
         for rapport in all_rapports:
             # print(rapport)
             button_dict[f'index_button{rapport["RAP_NUM"]}'] = QPushButton("afficher")
+            suppr_dict[f'index_suppr{rapport["RAP_NUM"]}'] = QPushButton("supprimer")
+            # test_dict[f'index_test{rapport["RAP_NUM"]}'] = QPushButton("supprimer")
             self.index_tableau_rapports.insertRow(self.index_tableau_rapports.rowCount())                
             self.index_tableau_rapports.setItem(self.index_tableau_rapports.rowCount()-1, 0, QtWidgets.QTableWidgetItem(rapport['RAP_DATE']))
             self.index_tableau_rapports.setItem(self.index_tableau_rapports.rowCount()-1, 1, QtWidgets.QTableWidgetItem(rapport['RAP_COMMENTAIRE']))
             self.index_tableau_rapports.setItem(self.index_tableau_rapports.rowCount()-1, 2, QtWidgets.QTableWidgetItem(rapport['RAP_MOTIF']))
             self.index_tableau_rapports.setItem(self.index_tableau_rapports.rowCount()-1, 3, QtWidgets.QTableWidgetItem(rapport['RAP_BILAN']))
             self.index_tableau_rapports.setCellWidget(self.index_tableau_rapports.rowCount()-1, 4, button_dict[f'index_button{rapport["RAP_NUM"]}'])
+            self.index_tableau_rapports.setCellWidget(self.index_tableau_rapports.rowCount()-1, 5, suppr_dict[f'index_suppr{rapport["RAP_NUM"]}'])
 
-            button_dict[list(button_dict)[-1]].clicked.connect(CreerPresentation(rapport["affiliate_med"]["MED_NOM"],"test","test2"))
-
+            # button_dict[f'index_button{rapport["RAP_NUM"]}'].clicked.connect(CreerPresentation("test","test2"))
+            suppr_dict[f'index_suppr{rapport["RAP_NUM"]}'].clicked.connect(self.suppr)
 
         # for boutton in button_dict:
         #     button_dict[boutton].clicked.connect(self.caca)
@@ -95,6 +99,11 @@ class Index_page(QtWidgets.QWidget):
 
     def goToRapport(self):
         appStack.setCurrentWidget(appStack.rapport_page)
+
+    def suppr():
+        
+        return True
+
 
 
 class Rapport_page(QtWidgets.QWidget):
