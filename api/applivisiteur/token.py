dacbbf327f1565f084c292d537928b5351df6875
FabioF9 <114165319+FabioF9@users.noreply.github.com>
2024-02-13 09:48:31 +0100

    Update token.py

--- api/applivisiteur/token.py
+++ api/applivisiteur/token.py
@@ -8,7 +8,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_access_token(data: dict, expires_delta: timedelta):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
